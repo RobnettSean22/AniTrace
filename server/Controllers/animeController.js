d0a40a067dc74a1792e06964a9b5af7f1978e952
RobnettSean22 <robnettsean22@gmail.com>
2020-01-31 01:10:08 +0900

    controller for anime reads. Watching and planning

--- server/Controllers/animeController.js
+++ server/Controllers/animeController.js
@@ -1,3 +1,21 @@
 module.exports = {
-  aniViewPlanning: (req, res, next) => {}
+  planningAniView: (req, res, next) => {
+    const db = req.app.get("db");
+    const { username } = req.params;
+    db.planning_read(username)
+      .then(planning => res.status(200).send(planning))
+      .catch(err => {
+        res.status(400).send({ errorMessage: "can not view planning" });
+      });
+  },
+
+  watchingAniView: (req, res, next) => {
+    const db = req.app.get("db");
+    const { username } = req.params;
+    db.watching_read(username)
+      .then(watching => res.status(200).send(watching))
+      .catch(err => {
+        res.status(400).send({ errorMessage: "can not view watching" });
+      });
+  }
 };
