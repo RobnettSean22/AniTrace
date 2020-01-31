3920a3cf72fb7beb43572e26c20806c5107d4bd9
RobnettSean22 <robnettsean22@gmail.com>
2020-02-01 01:31:46 +0900

    more work connecting data base to server side

--- server/Controllers/animeController.js
+++ server/Controllers/animeController.js
@@ -29,5 +29,39 @@ module.exports = {
       .catch(err => {
         res.status(400).send({ errorMessage: "can not view completed" });
       });
+  },
+  planningCreate: (req, res, next) => {
+    const db = rea.qpp.get("db");
+    const { username } = req.params;
+    const { plannig_code } = req.body;
+
+    db.planning_create([username, plannig_code])
+      .then(createPlanning => res.status(200).send(createPlanning))
+      .catch(err => {
+        res.status(400).send({ errorMessage: "can not creat planning" });
+      });
+  },
+
+  watchingCreate: (req, res, next) => {
+    const db = rea.qpp.get("db");
+    const { username } = req.params;
+    const { watching_code } = req.body;
+
+    db.planning_create([username, watching_code])
+      .then(createWatching => res.status(200).send(createWatching))
+      .catch(err => {
+        res.status(400).send({ errorMessage: "can not creat planning" });
+      });
+  },
+  completedCreate: (req, res, next) => {
+    const db = rea.qpp.get("db");
+    const { username } = req.params;
+    const { completed_code } = req.body;
+
+    db.completed_create([username, completed_code])
+      .then(createCompleted => res.status(200).send(createCompleted))
+      .catch(err => {
+        res.status(400).send({ errorMessage: "can not creat planning" });
+      });
   }
 };
